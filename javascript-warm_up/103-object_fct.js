0e7115ab0daaeeb19cef0febeddf350c12648188
yassine-fawzi <fawziyassine0@gmail.com>
2024-01-31 20:00:25 +0100

    push

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,16 +1,15 @@
 const myObject = {
     type: 'object',
-    value: 12,  
+    value: 12,
     incr() {
         this.value++;
     }
 };
 
-console.log({ type: myObject.type, value: myObject.value });
+console.log(`{ type: '${myObject.type}', value: ${myObject.value} }`);
 myObject.incr();
-console.log({ type: myObject.type, value: myObject.value, incr: [Function] });
+console.log(`{ type: '${myObject.type}', value: ${myObject.value}, incr: [Function] }`);
 myObject.incr();
-console.log({ type: myObject.type, value: myObject.value, incr: [Function] });
+console.log(`{ type: '${myObject.type}', value: ${myObject.value}, incr: [Function] }`);
 myObject.incr();
-console.log({ type: myObject.type, value: myObject.value, incr: [Function] });
-  
+console.log(`{ type: '${myObject.type}', value: ${myObject.value}, incr: [Function] }`);
---
+++ javascript-warm_up/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+const myObject = {
+    type: 'object',
+    value: 12,  
+    incr() {
+        this.value++;
+    }
+};
+
+console.log({ type: myObject.type, value: myObject.value });
+myObject.incr();
+console.log({ type: myObject.type, value: myObject.value, incr: myObject.incr });
+myObject.incr();
+console.log({ type: myObject.type, value: myObject.value, incr: myObject.incr });
+myObject.incr();
+console.log({ type: myObject.type, value: myObject.value, incr: myObject.incr });
+  
